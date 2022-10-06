55d1413cb21def23bc325ee42dacaf9a0cbdb460
Nesterenko Sophia <nykolay76@yandex.ru>
2022-10-06 13:55:20 +0200

    add program

---
+++ Module3HW1/IMyList.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Module3HW1
+{
+    public interface IMyList<T> : ICollection<T>
+    {
+        public void AddRange(IEnumerable<T> collection);
+        public void RemoveAt(int index);
+        public void Sort();
+    }
+}
---
+++ Module3HW1/ListComparer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Module3HW1
+{
+    public class ListComparer<T> : IComparer<T>
+    {
+        public int Compare(T x, T y)
+        {
+            if (x.GetHashCode() > y.GetHashCode())
+            {
+                return 1;
+            }
+            else if (x.GetHashCode() < y.GetHashCode())
+            {
+                return -1;
+            }
+
+            return 0;
+        }
+    }
+}
